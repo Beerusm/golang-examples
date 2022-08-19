8c72bdcef8c6888d3be8940a98be23b207e78116
Beerappa Shivappa Motebennur <beerappa.motebennur2@radisys.com>
2022-08-19 12:48:59 +0530

    delete a slice

--- 17-slice/main.go
+++ 17-slice/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	fmt.Println("Len:", len(slice1), "cap:", cap(slice1), "slice1:", slice1)
+
+	slice4, _ := sliceDelete(slice1, 8)
+	fmt.Println("Len:", len(slice4), "cap:", cap(slice4), "slice4 after delete index 8:", slice4)
+
 	slice1, _ = insert(slice1, 5, 500)
 	fmt.Println("Len:", len(slice1), "cap:", cap(slice1), "slice1:", slice1)
 	slice1, _ = insert(slice1, 0, 100)
@@ -42,6 +46,19 @@ func insert(slice []int, i, v int) ([]int, error) {
 
 	slice = append(slice, slice[0])
 	slice[0] = v
+	return slice, nil
+
+}
+
+func sliceDelete(slice []int, i int) ([]int, error) {
+	if slice == nil {
+		return nil, fmt.Errorf("nil slice")
+	}
+	if i < 0 && i > len(slice) {
+		return nil, fmt.Errorf("invalid index")
+	}
+	slice = append(slice[:i], slice[i+1:]...)
+	fmt.Println(slice)
 	return slice, nil
 
 }
